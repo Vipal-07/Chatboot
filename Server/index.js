1451bbcd380b56ff0d6446aa0121625f217b3689
Vikas Maurya <164004863+Vipal-07@users.noreply.github.com>
2025-06-10 23:12:25 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -49,7 +49,7 @@ main()
 
 const io = new Server(httpServer, {
     cors: {
-        origin: 'http://localhost:5173',
+        origin: 'https://chat-boot-9yl6.onrender.com',
         credentials: true,
     }
 });
